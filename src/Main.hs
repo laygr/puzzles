3dad624b154b7bd05baab5b651c6df38e5b30d83
laygr <lay.gr@me.com>
2014-07-08 08:31:25 +0000

    puzzle 1 amend

--- src/Main.hs
+++ src/Main.hs
@@ -8,7 +8,7 @@ import Control.Monad
 
 main = mapM print result
 
-digsToInt x = sum $ map (\pair -> snd pair * 10 ^ fst pair) (zip [3,2,1,0] x)
+listToInt x = sum $ map (\pair -> snd pair * 10 ^ fst pair) (zip [3,2,1,0] x)
 
 intToList x = reverse $ intToListAux x
 intToListAux 0 = []
@@ -19,17 +19,17 @@ elementsFromList code list = map (\n -> list !! n) code
 result = do
     permutation <- permutations [0..9]
     let
-                abcdList = elementsFromList [0,1,2,3] permutation
-                bcadList = elementsFromList [1,2,0,3] permutation
-                abcd = digsToInt abcdList
-                bcad = digsToInt bcadList
-                addition = abcd + bcad
-                additionList = intToList addition
-                f = additionList !! 0
-                a = additionList !! 1
-                e = additionList !! 2
-                c = additionList !! 3
-                in
-                    guard ( notElem f abcdList  && a == (abcdList !! 0)  &&
-                            notElem e abcdList  && c == (abcdList !! 2)  && f /= e)
-                          >> return (show abcd ++ "+" ++ show bcad ++ "=" ++ show addition)
+        abcdList = elementsFromList [0,1,2,3] permutation
+        bcadList = elementsFromList [1,2,0,3] permutation
+        abcd = listToInt abcdList
+        bcad = listToInt bcadList
+        addition = abcd + bcad
+        additionList = intToList addition
+        f = additionList !! 0
+        a = additionList !! 1
+        e = additionList !! 2
+        c = additionList !! 3
+            in
+                guard ( notElem f abcdList  && a == (abcdList !! 0)  &&
+                        notElem e abcdList  && c == (abcdList !! 2)  && f /= e)
+                      >> return (show abcd ++ "+" ++ show bcad ++ "=" ++ show addition)
